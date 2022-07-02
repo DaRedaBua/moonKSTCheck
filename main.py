564af6aa427021577a509770d9979ddc46fdf424
f.reder <Dim2.Pw!>
2022-07-02 20:42:05 +0200

    v1.0.1

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import datetime
 import Bimail
+import sys, os
+import traceback
 
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
-
-#whitePath = "C:/CMD/KST_CHECK/KST_EXPORT_" + yesterday.strftime('%Y_%m_%d') + ".csv"
-#importPath = "C:/LX_Import/" + yesterday.strftime('%Y-%m-%d') + "/"
-#aufKSTPath = importPath + "Import_Aufteilung_Kostenstellen_" + yesterday.strftime('%Y-%m-%d') + ".csv"
-#lohnartenPath = importPath + "Import_Lohnarten_" + yesterday.strftime('%Y-%m-%d') + ".csv"
-#zeitenPath = importPath + "Import_Zeiten_" + yesterday.strftime('%Y-%m-%d') + ".csv"
 
 whitePath = ''
 importPath = ''
 aufKSTPath = ''
 lohnartenPath = ''
 zeitenPath = ''
+
+outAufPath = []
+outLohnartenPath = []
+outZeitenPath = []
 
 recipients = []
 
@@ -24,48 +24,66 @@ whiteList = []
 
 files = {}
 
-#files = {"aufKST": [aufKSTPath, 4, 0, 1, 3],
-#         "lohnarten": [lohnartenPath, 14, 0, 1, 12],
-#         "zeiten": [zeitenPath, 8, 0, 1, 3]}
-
 msgs = []
 msg = ""
-teiler = "\n\n---------------------------------------------------------------\n\n"
+teiler = "<br><br>---------------------------------------------------------------<br><br>"
 
 readConfSuccess = True
 readWhiteSucess = True
 
 
-def checkImport(file, name):
+def checkImport(path, cols, name, outpath):
     global msg
+
+    msg += path + '<br>'
+    msg += str(cols) + '<br>'
+    msg += name + '<br>'
+    msg += str(outpath) + '<br>'
 
     new_rows = []
 
-    print("---------------------" + name + "-------------------------------")
+    opensuccess = False
+    replacesuccess = False
 
-    with open(file[0], 'r') as f:
-        csvlines = f.readlines()
+    msgs.append("<br>")
 
-        msg += "<br>Datei " + file[0] + "gefunden"
+    try:
+        with open(path, 'r') as f:
+            csvlines = f.readlines()
+    
+            msg += "<br>" + name + "-Datei erfolgreich geladen: <br>      " + path + '<br>'
+            msgs.append("<br>" + name + "-Datei erfolgreich geladen: <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + path + '<br>')
+            opensuccess = True
 
-    for row in range(0, len(csvlines)):
+    except Exception as e:
+        msg += "Konnte Datei " + name + " nicht öffnen - " + path + "<br>"
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        msg += str(exc_type, fname, str(exc_tb.tb_lineno)) + '<br>'
 
-        new_row = csvlines[row]
-        elems = csvlines[row].split(';')
+    msg += "Opensuccess: " + str(opensuccess) + '<br>'
 
-        #Erste Zeile (Header) Auslassen
-        if row != 0:
+    #try:
+    if opensuccess:
+        msg += "Starte Ersetzungsvorgang" + "<br>"
+        msgs.append("&nbsp&nbsp&nbsp&nbsp&nbsp&nbspStarte Ersetzunngsvorgang<br>")
 
-            if elems[file[1]] not in whiteList:
-                print(elems[file[1]] + " is NOT whitelisted")
-                msgs.append(name + "  -- Firma: " + elems[file[2]] + " PersNR: " + elems[file[3]] + " Tag: " + elems[file[4]] + " -- KST " + elems[file[1]] + " nicht gefunden! <br>")
+        for row in range(1, len(csvlines)):
+            new_row = csvlines[row]
+            #msg += new_row + '<br>'
+
+            elems = csvlines[row].split(';')
+
+            if elems[cols[0]] not in whiteList:
+                msgs.append(name + "  -- Firma: " + elems[cols[1]] + " PersNR: " + elems[cols[2]] + " Tag: " + elems[cols[3]] + " -- KST " + elems[cols[0]] + " nicht gefunden - KST nicht in Whitelist! <br>")
+                msg += name + "  -- Firma: " + elems[cols[1]] + " PersNR: " + elems[cols[2]] + " Tag: " + elems[cols[3]] + " -- KST " + elems[cols[0]] + " nicht gefunden - KST nicht in Whitelist! <br>"
 
                 count = 0
                 new_row = ""
 
                 #Zeile neu basteln, nur fündiges ersetzten durch DUMMY
                 for e in elems:
-                    if count != file[1]:
+                    if count != cols[0]:
                         new_row = new_row + e.rstrip()
                     else:
                         new_row = new_row + "DUMMY"
@@ -75,13 +93,52 @@ def checkImport(file, name):
                     if count != len(elems):
                         new_row = new_row + ";"
                     else:
-                        new_row = new_row + "\n"
+                        new_row = new_row + "<br>"
 
-        new_rows.append(new_row)
+            new_rows.append(new_row)
 
-    with open(file[0], 'w') as f:
-        for row in new_rows:
-            f.write(row)
+        msg += "<br><br>Ersetzungsvorgang für " + name + " abgeschlossen"
+        replacesuccess = True
+
+    msg += "<br>Replacesuccess: " + str(replacesuccess) + '<br>'
+
+    if replacesuccess:
+        try:
+            msg += "Schreibe datei in outpath - " + outpath + "<br>"
+
+            with open(outpath , 'w') as f:
+                for row in new_rows:
+                    f.write(row)
+
+            f.close()
+
+            msgs.append("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + name + "-Datei erfolgreich gespeichert:<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + outpath + '<br>')
+            msg += name + "-Datei erfolgreich gespeichert:<br>      " + outpath
+
+        except Exception as e:
+            print(outpath)
+            msg += "Konnte Datei nicht speichern - ECHT " + outpath
+            msg += traceback.format_exc()
+
+        try:
+            msg += "Schreibe Daten zurück in ouriginaldatei - " + path
+
+            with open(path, 'w') as f:
+                for row in new_rows:
+                    f.write(row)
+
+            f.close()
+
+            msgs.append("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + name + "-Datei erfolgreich zurückgeschrieben:<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + path)
+            msg += name + "-Datei erfolgreich zurückgeschrieben:<br>      " + path
+
+        except Exception as e:
+            msg += "Konnte Datei nicht speichern - ORIGINALDATEI " + path
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            msg += str(exc_type, fname, str(exc_tb.tb_lineno)) + '<br>'
+
+        msg += teiler
 
 
 def readWhitelist():
@@ -89,7 +146,7 @@ def readWhitelist():
     global msg
     global readWhiteSucess
 
-    msg += "Versuche Whitlist zu laden - " + whitePath + "\n"
+    msg += "Versuche Whitlist zu laden - " + whitePath + "<br>"
 
     try:
         with open(whitePath) as f:
@@ -100,39 +157,49 @@ def readWhitelist():
 
                 whiteList.append(row[0])
 
-            msg += "Whitelist erfolgreich gelesen - " + whitePath + "\n"
+            msg += "Whitelist erfolgreich gelesen - " + whitePath + "<br>"
+            msgs.append("Whitelist erfolgreich gelesen - " + whitePath + "<br>")
+            msg += str(whiteList)
             msg += f.read()
-            msg += teiler
 
     except Exception as e:
-        msg += "Whitelist konnte nicht gelesen werden!\n Programm wird beendet"
-        msg + str(e)
+        msg += "Whitelist konnte nicht gelesen werden!<br> Programm wird beendet"
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        msg += str(exc_type, fname, str(exc_tb.tb_lineno)) + '<br>'
         readWhiteSucess = False
+
+    finally:
+        msg += teiler
 
 
 def sendEmail():
-    mymail = Bimail.Bimail(subj, recipients)
-    text = ""
+    global msg
 
-    for msg in msgs:
-        text = text + msg
+    try:
+        msg += "Sende Email an " + str(recipients) + "..."
 
-    text = text + "<br><br>Alle KST durch 'DUMMY' ersetzt."
+        mymail = Bimail.Bimail(subj, recipients)
+        text = ""
 
-    print(text)
+        for smsg in msgs:
+            text = text + smsg
 
-    mymail.htmladd(text)
+        msg = text + teiler + msg
 
-    mymail.send()
+        mymail.htmladd(msg)
 
-# LEGACY
-# def loadEmailRecipients():
-#    with open("C:/CMD/moonKSTcheckMail.csv") as f:
-#        csvlines = f.readlines()
-#        for x in range(0, len(csvlines)):
-#            recipients.append(csvlines[x].rstrip())
-#
-#    print(recipients)
+        mymail.send()
+
+    except Exception as e:
+        msg += "Konnte E-Mail nicht versenden..."
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        msg += str(exc_type, fname, str(exc_tb.tb_lineno)) + '<br>'
+
+    finally:
+        msg += teiler
+
 
 def loadConfig():
     global recipients
@@ -142,68 +209,110 @@ def loadConfig():
     global lohnartenPath
     global zeitenPath
     global readConfSuccess
+    global outLohnartenPath
+    global outZeitenPath
+    global outAufPath
     global msg
 
     today = datetime.date.today()
 
-    msg += "Versuche config-File zu lesen - ./config.csv\n\n"
+    msg += "Versuche config-File zu lesen - ./config.csv<br><br>"
 
     try:
         with open('./config.csv', 'r') as f:
             lines = f.readlines()
             for x in range(len(lines)):
-                lineElems = lines[x].split(';')
+
+                lineElems = str.rstrip(lines[x]).split(';')
 
                 #EmailEmpfänger einlesen - ganze Zeile, adressen getrennt durch ;
                 if x == 0:
                     recipients = lineElems
-                    msg += str(recipients) + '\n'
+                    recipients = list(filter(None, recipients))
+                    msg += str(recipients) + '<br>'
 
                 if x == 1:
                     day = today - datetime.timedelta(days=-1*int(lineElems[2]))
                     whitePath = lineElems[1] + day.strftime(lineElems[3]) + lineElems[4]
-                    msg += whitePath + '\n'
+                    msg += whitePath + '<br>'
 
                 if x == 2:
                     day = today - datetime.timedelta(days=-1*int(lineElems[2]))
                     importPath = lineElems[1] + day.strftime(lineElems[3]) + lineElems[4]
-                    msg += importPath + '\n'
+                    msg += importPath + '<br>'
 
                 if x == 3:
                     day = today - datetime.timedelta(days=-1*int(lineElems[2]))
                     aufKSTPath = importPath + lineElems[1] + day.strftime(lineElems[3]) + lineElems[4]
-                    msg += aufKSTPath + '\n'
+                    msg += aufKSTPath + '<br>'
 
                 if x == 4:
                     day = today - datetime.timedelta(days=-1*int(lineElems[2]))
                     lohnartenPath = importPath + lineElems[1] + day.strftime(lineElems[3]) + lineElems[4]
-                    msg += lohnartenPath + '\n'
+                    msg += lohnartenPath + '<br>'
 
                 if x == 5:
                     day = today - datetime.timedelta(days=-1*int(lineElems[2]))
                     zeitenPath = importPath + lineElems[1] + day.strftime(lineElems[3]) + lineElems[4]
                     msg += zeitenPath
 
-                #if x == 6: überschrift für Colums
+                # if x == 6: überschrift für Colums
 
                 if x == 7 or x == 8 or x == 9:
-                    files[lineElems[0]] = [lineElems[1], lineElems[2], lineElems[3], lineElems[4]]
+                    files[lineElems[0]] = [int(lineElems[1]), int(lineElems[2]), int(lineElems[3]), int(lineElems[4])]
 
                 # nur für msg DOKU
                 if x == 9:
-                    msg += str(files) + '\n'
+                    msg += str(files) + '<br>'
+
+                # if x == 10: überschrift für outFiles
+
+                if x == 11:
+                    day = today - datetime.timedelta(days=-1 * int(lineElems[2]))
+                    outAufPath = lineElems[1] + '/' + lineElems[0] + day.strftime(lineElems[3]) + lineElems[4]
+                    msg += outAufPath
+
+                if x == 12:
+                    day = today - datetime.timedelta(days=-1 * int(lineElems[2]))
+                    outLohnartenPath = lineElems[1] + '/' + lineElems[0] + day.strftime(lineElems[3]) + lineElems[4]
+                    msg += outLohnartenPath
+
+                if x == 13:
+                    day = today - datetime.timedelta(days=-1 * int(lineElems[2]))
+                    outZeitenPath = lineElems[1] + '/' + lineElems[0] + day.strftime(lineElems[3]) + lineElems[4]
+                    msg += outZeitenPath
 
         msg += "Config geladen"
+        msgs.append("Config File geladen - ./config.csv <br><br>")
+
+        msg += "recipients - " + str(recipients)
+        msg += "whitelistPath - " + whitePath
+        msg += "importPath - " + importPath
+        msg += "aufKSTPath - " + aufKSTPath
+        msg += "lohnartenPath - " + lohnartenPath
+        msg += "zeitenPath - " + zeitenPath + '<br>'
+        msg += str(files) + '<br>'
+        msg += str(outAufPath) + '<br>'
+        msg += str(outLohnartenPath) + '<br>'
+        msg += str(outZeitenPath) + '<br>'
+
         msg += teiler
+
 
     except Exception as e:
 
-        msg += "Konnte Config nicht in Variablen laden\n"
-        msg += str(e)
+        msg += "Konnte Config nicht in Variablen laden<br>"
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        msg += str(exc_type, fname, str(exc_tb.tb_lineno)) + '<br>'
         msg += teiler
         readConfSuccess = False
 
+
 def main():
+
+    global msg
+
 
     loadConfig()
 
@@ -211,12 +320,13 @@ def main():
         readWhitelist()
 
     if readWhiteSucess and readConfSuccess:
-        checkImport(files["aufteilungFile"], "Aufteilung Kostenstellen")
-        checkImport(files["lohnartenFile"], "Lohnarten")
-        #checkImport(files["zeitenFile"], "Zeiten")
+        checkImport(aufKSTPath, files["aufteilungKSTs"], "Aufteilung Kostenstellen", outAufPath)
+        checkImport(lohnartenPath, files["lohnarten"], "Lohnarten", outLohnartenPath)
+        #checkImport(zeitenPath, files["zeiten"], "Zeiten", outZeitenPath)
 
-    #sendEmail()
+    sendEmail()
 
+    msg = msg.replace('<br>', '\n')
     print(msg)
 
 main()
